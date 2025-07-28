3d9207cdcb241baed66f898401bdd5b6d2af09ba
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2025-07-29 05:20:18 +1000

    Add description to all projects page

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,14 +1,21 @@
 <script lang="ts">
-	import { ArrowLeft } from '@lucide/svelte';
+	import { ArrowLeft, ChevronUp, Minus, Plus } from '@lucide/svelte';
+	import { SvelteSet } from 'svelte/reactivity';
+	import { fade, fly, scale, slide } from 'svelte/transition';
 
 	import { PROJECTS } from '$lib/assets/content';
 	import Badge from '$lib/components/Badge.svelte';
+	import Button from '$lib/components/Button.svelte';
 	import Link from '$lib/components/Link.svelte';
+	import Project from '$lib/components/Project.svelte';
+	import Projects from '$lib/components/Projects.svelte';
 	import { formatMonthYear } from '$lib/utils/dates';
 	import { domain } from '$lib/utils/url';
+
+	let expanded = $state<number | undefined>();
 </script>
 
-<div
+<main
 	class="flex flex-col justify-center gap-4 min-h-screen max-w-screen-xl p-10 transition-all duration-300 mx-auto"
 >
 	<a href="/" class="self-start flex gap-2 items-center anchor"><ArrowLeft />Daniel Brown</a>
@@ -18,17 +25,18 @@
 		<table class="table caption-bottom">
 			<thead>
 				<tr>
-					<th>Year</th>
-					<th>Project</th>
-					<th>Made At</th>
-					<th>Skills</th>
-					<th>Links</th>
+					<th class="w-[12%]">Date</th>
+					<th class="w-[12%]">Project</th>
+					<th class="w-[12%]">Made At</th>
+					<th class="w-[52%]">Skills</th>
+					<th class="w-[12%]">Links</th>
 				</tr>
 			</thead>
 
 			<tbody class="[&>tr]:hover:preset-tonal-secondary">
-				{#each PROJECTS as { name, date, madeAt, skills = [], url, sourceUrl }}
-					<tr>
+				{#each PROJECTS as { name, date, madeAt, skills = [], url, sourceUrl, description }, idx}
+					{@const isExpanded = expanded === idx}
+					<tr class="cursor-pointer" onclick={() => (expanded = isExpanded ? undefined : idx)}>
 						<td>{formatMonthYear(date)}</td>
 						<td>{name}</td>
 						<td>{madeAt}</td>
@@ -40,21 +48,31 @@
 							</div>
 						</td>
 						<td>
-							<div class="flex flex-col flex-1 gap-2">
-								{#each [url, sourceUrl] as link}
-									{#if link}
-										<Link href={link} target="_blank">
-											{domain(link)}
-										</Link>
-									{/if}
-								{/each}
-							</div>
+							{#each [url, sourceUrl] as link}
+								{#if link}
+									<Link href={link} target="_blank">
+										{domain(link)}
+									</Link>
+								{/if}
+							{/each}
 						</td>
 					</tr>
+					{#if isExpanded}
+						<tr
+							class="border-t-0"
+							transition:fly={{ duration: 300, y: -20, opacity: 0 }}
+							onclick={() => (expanded = undefined)}
+						>
+							<td colspan="5">
+								<ChevronUp class="mx-auto" />
+								<p class="text-sm">{description}</p>
+							</td>
+						</tr>
+					{/if}
 				{/each}
 			</tbody>
 
 			<caption class="pt-4">* Most projects done at work do not have a Source URL.</caption>
 		</table>
 	</div>
-</div>
+</main>
