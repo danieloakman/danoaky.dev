a2c193be92d6b5b377e2eaedd318df47d20a27eb
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2025-07-30 03:56:56 +1000

    Turn on prerender/static

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,18 @@
 import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter(),
+		prerender: {
+			handleHttpError: ({ path, referrer, message }) => {
+				if (path === '/resume') return;
+				throw new Error(message);
+			}
+		}
+	}
 };
 
 export default config;
